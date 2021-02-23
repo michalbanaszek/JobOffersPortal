1ed088aae3fd721ac3649ef71fd152dcf8604213
Michał Banaszek <biaaly13@outlook.com>
2021-02-23 18:16:56 +0100

    added new list in constructor

--- JobOffersPortal.WebUI/Domain/Company.cs
+++ JobOffersPortal.WebUI/Domain/Company.cs
@@ -1,15 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.Collections.Generic;
 
 namespace JobOffersPortal.WebUI.Domain
 {
     public class Company : BaseModel
     {
+        public Company()
+        {
+            JobOffers = new List<CompanyJobOffer>();
+        }
+
         public string Id { get; set; }
         public string Name { get; set; }      
 
         //public string City { get; set; }
+
         //public string AboutUs { get; set; }
 
         public virtual List<CompanyJobOffer> JobOffers { get; set; }
