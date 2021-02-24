096f03cb4b8acf0e2f5a1a57cfd3cab1054b627d
Michał Banaszek <biaaly13@outlook.com>
2021-02-24 19:51:33 +0100

    added integration tests

---
+++ JobOffersPortal.IntegrationTests/ControllersTest/CompanyControllerTests.cs
@@ -0,0 +1,101 @@
+﻿using FluentAssertions;
+using JobOffersPortal.Contracts.Contracts.Responses;
+using JobOffersPortal.WebUI;
+using JobOffersPortal.WebUI.Contracts.Requests;
+using JobOffersPortal.WebUI.Contracts.Responses;
+using JobOffersPortal.WebUI.Domain;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace JobOffersPortal.IntegrationTests.ControllersTest
+{
+    public class CompanyControllerTests : IntegrationTest
+    {
+        [Fact]
+        public async Task GetAll_WithoutAnyCompanies_ReturnsEmptyResponse()
+        {
+            // Arrange
+            await AuthenticateAsync();
+
+            // Act
+            var response = await _httpClient.GetAsync(ApiRoutes.Company.GetAll);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await response.Content.ReadAsAsync<PagedResponse<Company>>()).Data.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task Get_ReturnsCompany_WhenCompanyExistsInTheDatabase()
+        {
+            // Arrange
+            await AuthenticateAsync();
+            var createdCompany = await CreateCompanyAsync(new CreateCompanyRequest() { Name = "Test Company" });
+
+            // Act
+            var response = await _httpClient.GetAsync(ApiRoutes.Company.Get.Replace("{id}", createdCompany.Data.Id));
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var returnedCompany = await response.Content.ReadAsAsync<Response<CompanyResponse>>();
+            returnedCompany.Data.Id.Should().Be(createdCompany.Data.Id);
+            returnedCompany.Data.Name.Should().Be("Test Company");
+        }
+
+        [Fact]
+        public async Task Update_ReturnsUpdatedCompany_WhenCompanyExistsInTheDatabase()
+        {
+            // Arrange
+            await AuthenticateAsync();
+            var createdCompany = await CreateCompanyAsync(new CreateCompanyRequest() { Name = "Test Company" });
+            var updatedCompany = new UpdateCompanyRequest()
+            {
+                Name = "Updated Company"
+            };
+
+            // Act
+            var response = await _httpClient.PutAsJsonAsync(ApiRoutes.Company.Update.Replace("{id}", createdCompany.Data.Id), updatedCompany);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var returnedCompany = await response.Content.ReadAsAsync<Response<CompanyResponse>>();
+            returnedCompany.Data.Id.Should().Be(createdCompany.Data.Id);
+            returnedCompany.Data.Name.Should().Be(updatedCompany.Name);
+        }
+
+        [Fact]
+        public async Task Create_ReturnsCreatedCompany()
+        {
+            // Arrange
+            await AuthenticateAsync();
+            var createdCompany = new CreateCompanyRequest() { Name = "Test Company" };
+         
+            // Act
+            var response = await _httpClient.PostAsJsonAsync(ApiRoutes.Company.Create, createdCompany);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+            var returnedCompany = await response.Content.ReadAsAsync<Response<CompanyResponse>>();
+            returnedCompany.Data.Name.Should().Be(createdCompany.Name);
+        }
+
+        [Fact]
+        public async Task Delete_ReturnsEmptyResponse_WhenCompanyExistsInTheDatabase()
+        {
+            // Arrange
+            await AuthenticateAsync();
+            var createdCompanyRequest = new CreateCompanyRequest() { Name = "Test Company" };
+            var createdCompanyResponse = await CreateCompanyAsync(createdCompanyRequest);
+
+            // Act
+            var response = await _httpClient.DeleteAsync(ApiRoutes.Company.Delete.Replace("{id}", createdCompanyResponse.Data.Id));
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NoContent);            
+        }
+    }
+}
---
+++ JobOffersPortal.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,83 @@
+﻿using JobOffersPortal.Contracts.Contracts.Responses;
+using JobOffersPortal.WebUI;
+using JobOffersPortal.WebUI.Contracts.Requests;
+using JobOffersPortal.WebUI.Contracts.Responses;
+using JobOffersPortal.WebUI.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace JobOffersPortal.IntegrationTests
+{
+    public class IntegrationTest : IDisposable
+    {
+        protected readonly HttpClient _httpClient;
+        private readonly IServiceProvider _serviceProvider;
+        private object _lockObj = new object();
+
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<DataContext>));
+
+                        if (descriptor != null)
+                        {
+                            services.Remove(descriptor);
+                        }
+
+                        services.AddDbContext<DataContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDbInMemory");
+                        });
+                    });
+                });
+
+            _serviceProvider = appFactory.Services;
+            _httpClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await GetJwtAsync());
+        }
+
+        protected async Task<Response<CompanyResponse>> CreateCompanyAsync(CreateCompanyRequest request)
+        {
+            var response = await _httpClient.PostAsJsonAsync(ApiRoutes.Company.Create, request);
+
+            return await response.Content.ReadAsAsync<Response<CompanyResponse>>();
+        }
+
+
+        private async Task<string> GetJwtAsync()
+        {
+            var response = await _httpClient.PostAsJsonAsync(ApiRoutes.Identity.Register, new RegisterRequest()
+            {
+                Email = "test123@gmail.com",
+                Password = "Qwerty!1"
+            });
+
+            var registrationResponse = await response.Content.ReadAsAsync<AuthSuccessResponse>();
+
+
+            return registrationResponse.Token;
+        }
+
+        public void Dispose()
+        {            
+                using var serviceScope = _serviceProvider.CreateScope();
+                var context = serviceScope.ServiceProvider.GetService<DataContext>();
+                context.Database.EnsureDeleted();
+        }
+    }
+}
