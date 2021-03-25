5d46b92e3d2ca900075c599517f7c3dd6490b131
Michał Banaszek <biaaly13@outlook.com>
2021-03-25 11:18:36 +0100

    updated DI

---
+++ Infrastructure/DependencyInjections/HealthChecksInstaller.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Options;
+using Infrastructure.Persistence;
+using Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.DependencyInjections
+{
+    public class HealthChecksInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHealthChecks().AddDbContextCheck<ApplicationDbContext>();
+
+            var redisCacheSettings = new RedisCacheOptions();
+            configuration.GetSection(nameof(RedisCacheOptions)).Bind(redisCacheSettings);
+            services.AddSingleton(redisCacheSettings);
+
+            if (!redisCacheSettings.Enabled)
+            {
+                return;
+            }
+
+            services.AddStackExchangeRedisCache(options => options.Configuration = redisCacheSettings.ConnectionString);
+            services.AddSingleton<IResponseCacheService, ResponseCacheService>();
+        }
+    }
+}
