a3feac017a633f483463874a44a4b65950c9e146
Michał Banaszek <biaaly13@gmail.com>
2021-10-08 10:39:31 +0200

    updated seed data

--- JobOffersPortal.Infrastructure/Persistence/ApplicationDbContextSeed.cs
+++ JobOffersPortal.Infrastructure/Persistence/ApplicationDbContextSeed.cs
@@ -87,10 +87,12 @@ namespace Infrastructure.Persistence
         {
             var positions = new[]
             {
-                "Tlumacz angielskiego",
+                "TŁUMACZ NIEMIECKIEGO",
                 "Pakowacz",
                 "Slusarz",
-                "Murarz"
+                "Murarz",
+                "MAGAZYNIER BLACH",
+                "MAGAZYNIER"
             };
 
             var skills = new[]
@@ -108,16 +110,16 @@ namespace Infrastructure.Persistence
             };
 
             var requirements = new[]
-            {
+       {
                 "bardzo dobra znajomość języka niemieckiego w mowie i piśmie",
                 "mile widziane doświadczenie w zarządzaniu projektami",
                 "kreatywność oraz dobra organizacja pracy",
                 "wykształcenie min. zawodowe",
-                "doświadczenie zawodowe na wyżej wymienionym stanowisku"
+                "doświadczenie zawodowe na wyżej wymienionym stanowisku",
             };
 
             var propositions = new[]
-            {
+       {
                 "umowę o pracę na pełen etat",
                 "odpowiednie do stanowiska narzędzia pracy",
                 "atrakcyjne wynagrodzenie",
@@ -127,7 +129,53 @@ namespace Infrastructure.Persistence
                 "stałą pracę w firmie o uregulowanej pozycji rynkowej"
             };
 
-            var salaries = new[] { 2000, 2500, 3000, 3200, 4000, 5000 };
+            var skills2 = new[]
+         {
+                "przyjmowanie towarów do magazynu",
+                "uczestnictwo w odbiorze ilościowym i jakościowym",
+                "sporządzanie odpowiedniej dokumentacji",
+                "prowadzenie dokumentacji magazynowej przy użyciu programów komputerowych lub metodami tradycyjnymi"
+            };
+
+            var requirements2 = new[]
+          {
+                "konieczne uprawnienia na wózki z doświadczeniem jazdy",
+                "podstawowa znajomość obiegu dokumentacji magazynowej"
+            };
+
+            var skills3 = new[]
+    {
+                "Obsługa komputera i programów magazynowych",
+                "Przyjęcie dostaw stali do magazynu",
+                "Sprawdzanie zgodności dokumentów dostawy z dostarczonym towarem",
+                "Wydawanie materiału na produkcje",
+                "Stała kontrola stanów magazynowych",
+                "Przestrzeganie zasad obiegu dokumentów",
+                "Dbałość o materiały, o czystość oraz higienę w magazynie"
+            };
+
+            var requirements3 = new[]
+          {
+                "Wykształcenie minimum zawodowe",
+                "Uprawnienia na wózki widłowe",
+                "Znajomość podstawowych materiałów stalowych takich jak profile, blachy, pręty",
+                "Dobra kondycja fizyczna",
+                "Dyspozycyjność, dokładność i odpowiedzialność",
+                "Umiejętność pracy w zespole, chęć i gotowość do intensywnej pracy",
+                "Precyzja i rzetelność w wykonywaniu powierzonych zadań"
+            };
+
+            var propositions3 = new[]
+            {
+                "Stałą pracę w firmie o uregulowanej pozycji rynkowej",
+                "Odpowiednie do stanowiska narzędzia pracy",
+                "Atrakcyjne wynagrodzenie",
+                "Stałość w terminach wypłat",
+                "Dobre warunki pracy",
+                "Możliwość rozwoju zawodowego"
+            };
+
+            var salaries = new[] { "2000", "2500", "3000", "3200", "4000", "5000" };
 
             var user1 = new ApplicationUser() { Email = "user1@gmail.com", UserName = "user1@gmail.com" };
             var user2 = new ApplicationUser() { Email = "user2@gmail.com", UserName = "user2@gmail.com" };
@@ -148,7 +196,7 @@ namespace Infrastructure.Persistence
             {
                 new Company()
             {
-                Id = Guid.NewGuid().ToString(),
+                Id = "0f2de8b6-9160-4843-b36e-90372a3f8179",
                 Name = "Company1",
                 CreatedBy = user1.Id,
                 Created = DateTime.Now
@@ -170,31 +218,95 @@ namespace Infrastructure.Persistence
                 Id = Guid.NewGuid().ToString(),
                 CompanyId = companies[0].Id,
                 Date = DateTime.Now,
+                IsAvailable = true,
                 Propositions = new List<JobOfferProposition>()
                 {
-                    new JobOfferProposition() { Id = Guid.NewGuid().ToString(), Content = propositions[0] },
-                    new JobOfferProposition() { Id = Guid.NewGuid().ToString(), Content = propositions[1] },
+                    new JobOfferProposition() { Id = Guid.NewGuid().ToString(), Content = propositions3[0] },
+                    new JobOfferProposition() { Id = Guid.NewGuid().ToString(), Content = propositions3[1] },
+                      new JobOfferProposition() { Id = Guid.NewGuid().ToString(), Content = propositions3[2] },
+                      new JobOfferProposition() { Id = Guid.NewGuid().ToString(), Content = propositions3[3] },
+                        new JobOfferProposition() { Id = Guid.NewGuid().ToString(), Content = propositions3[4] },
+                          new JobOfferProposition() { Id = Guid.NewGuid().ToString(), Content = propositions3[5] },
                 },
                 Requirements = new List<JobOfferRequirement>()
                 {
-                    new JobOfferRequirement() { Id = Guid.NewGuid().ToString(), Content = requirements[0] },
-                    new JobOfferRequirement() { Id = Guid.NewGuid().ToString(), Content = requirements[1] },
+                    new JobOfferRequirement() { Id = Guid.NewGuid().ToString(), Content = requirements3[0] },
+                    new JobOfferRequirement() { Id = Guid.NewGuid().ToString(), Content = requirements3[1] },
+                         new JobOfferRequirement() { Id = Guid.NewGuid().ToString(), Content = requirements3[2] },
+                    new JobOfferRequirement() { Id = Guid.NewGuid().ToString(), Content = requirements3[3] },
+                         new JobOfferRequirement() { Id = Guid.NewGuid().ToString(), Content = requirements3[4] },
+                    new JobOfferRequirement() { Id = Guid.NewGuid().ToString(), Content = requirements3[5] },
+                     new JobOfferRequirement() { Id = Guid.NewGuid().ToString(), Content = requirements3[6] },
                 },
                 Skills = new List<JobOfferSkill>()
                 {
-                    new JobOfferSkill() { Id = Guid.NewGuid().ToString(), Content = skills[0] },
-                    new JobOfferSkill() { Id = Guid.NewGuid().ToString(), Content = skills[1] },
+                    new JobOfferSkill() { Id = Guid.NewGuid().ToString(), Content = skills3[0] },
+                    new JobOfferSkill() { Id = Guid.NewGuid().ToString(), Content = skills3[1] },
+                       new JobOfferSkill() { Id = Guid.NewGuid().ToString(), Content = skills3[2] },
+                    new JobOfferSkill() { Id = Guid.NewGuid().ToString(), Content = skills3[3] },
+                       new JobOfferSkill() { Id = Guid.NewGuid().ToString(), Content = skills3[4] },
+                    new JobOfferSkill() { Id = Guid.NewGuid().ToString(), Content = skills3[5] },
+                       new JobOfferSkill() { Id = Guid.NewGuid().ToString(), Content = skills3[6] }
+
+                },
+                Salary = salaries[0],
+                Position = positions[4],
+                CreatedBy = user1.Id,
+                Created = DateTime.Now
+            },
+             new JobOffer()
+            {
+                Id = Guid.NewGuid().ToString(),
+                CompanyId = companies[0].Id,
+                Date = DateTime.Now,
+                IsAvailable = true,
+                    Propositions = new List<JobOfferProposition>()
+                {
+                    new JobOfferProposition() { Id = Guid.NewGuid().ToString(), Content = propositions3[0] },
+                    new JobOfferProposition() { Id = Guid.NewGuid().ToString(), Content = propositions3[1] },
+                },
+                Requirements = new List<JobOfferRequirement>()
+                {
+                    new JobOfferRequirement() { Id = Guid.NewGuid().ToString(), Content = requirements3[0] },
+                    new JobOfferRequirement() { Id = Guid.NewGuid().ToString(), Content = requirements3[1] },
+                },
+                Skills = new List<JobOfferSkill>()
+                {
+                    new JobOfferSkill() { Id = Guid.NewGuid().ToString(), Content = skills3[0] },
+                    new JobOfferSkill() { Id = Guid.NewGuid().ToString(), Content = skills3[1] },
+                },
+                Salary = salaries[0],
+                Position = positions[5],
+                CreatedBy = user1.Id,
+                Created = DateTime.Now
+            },
+             new JobOffer()
+            {
+                Id = Guid.NewGuid().ToString(),
+                CompanyId = companies[0].Id,
+                Date = DateTime.Now,
+                IsAvailable = true,
+                Requirements = new List<JobOfferRequirement>()
+                {
+                    new JobOfferRequirement() { Id = Guid.NewGuid().ToString(), Content = requirements2[0] },
+                    new JobOfferRequirement() { Id = Guid.NewGuid().ToString(), Content = requirements2[1] },
+                },
+                Skills = new List<JobOfferSkill>()
+                {
+                    new JobOfferSkill() { Id = Guid.NewGuid().ToString(), Content = skills2[0] },
+                    new JobOfferSkill() { Id = Guid.NewGuid().ToString(), Content = skills2[1] },
                 },
                 Salary = salaries[0],
                 Position = positions[0],
                 CreatedBy = user1.Id,
                 Created = DateTime.Now
             },
-              new JobOffer()
+             new JobOffer()
             {
                 Id = Guid.NewGuid().ToString(),
                 CompanyId = companies[1].Id,
                 Date = DateTime.Now,
+                IsAvailable = true,
                  Propositions = new List<JobOfferProposition>()
                 {
                     new JobOfferProposition() { Id = Guid.NewGuid().ToString(), Content = propositions[2] },
@@ -215,11 +327,12 @@ namespace Infrastructure.Persistence
                 CreatedBy = user1.Id,
                 Created = DateTime.Now
             },
-              new JobOffer()
+             new JobOffer()
             {
                 Id = Guid.NewGuid().ToString(),
                 CompanyId = companies[1].Id,
                 Date = DateTime.Now,
+                IsAvailable = true,
                  Propositions = new List<JobOfferProposition>()
                 {
                     new JobOfferProposition() { Id = Guid.NewGuid().ToString(), Content = propositions[4] },
