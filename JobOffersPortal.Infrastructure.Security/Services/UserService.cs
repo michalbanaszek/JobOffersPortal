fa8a8137f273fc881f0404e4b2333a6fa0baad3f
Michał Banaszek <biaaly13@gmail.com>
2022-01-19 14:18:20 +0100

    added user service

---
+++ JobOffersPortal.Infrastructure.Security/Services/UserService.cs
@@ -0,0 +1,65 @@
+﻿using JobOffersPortal.Application.Common.Interfaces;
+using JobOffersPortal.Infrastructure.Security.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobOffersPortal.Infrastructure.Security.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<string> GetUserNameAsync(string userId)
+        {
+            var user = await _userManager.Users.FirstAsync(u => u.Id == userId);
+
+            return user.UserName;
+        }
+
+        public async Task<bool> CreateUserAsync(string userName, string password)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = userName,
+                Email = userName,
+            };
+
+            var result = await _userManager.CreateAsync(user, password);
+
+            return result.Succeeded ? true : false;
+        }
+
+        public async Task<bool> IsInRoleAsync(string userId, string role)
+        {
+            var user = _userManager.Users.SingleOrDefault(u => u.Id == userId);
+
+            return await _userManager.IsInRoleAsync(user, role);
+        }
+
+        public async Task<bool> DeleteUserAsync(string userId)
+        {
+            var user = _userManager.Users.SingleOrDefault(u => u.Id == userId);
+
+            if (user != null)
+            {
+                return await DeleteUserAsync(user);
+            }
+
+            return false;
+        }
+
+        public async Task<bool> DeleteUserAsync(ApplicationUser user)
+        {
+            var result = await _userManager.DeleteAsync(user);
+
+            return result.Succeeded ? true : false;
+        }
+    }
+}
