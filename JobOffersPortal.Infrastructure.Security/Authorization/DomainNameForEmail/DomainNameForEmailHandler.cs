e3483e1a4264e1be28b06b9814307961ac55351f
Michał Banaszek <biaaly13@gmail.com>
2022-01-19 14:17:44 +0100

    change name classes

---
+++ JobOffersPortal.Infrastructure.Security/Authorization/DomainNameForEmail/DomainNameForEmailHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace JobOffersPortal.Infrastructure.Security.Authorization.DomainNameForEmail
+{
+    public class DomainNameForEmailHandler : AuthorizationHandler<DomainNameForEmailRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, DomainNameForEmailRequirement requirement)
+        {
+            var userEmailAddress = context.User?.FindFirstValue(ClaimTypes.Email) ?? string.Empty;
+
+            if (userEmailAddress.EndsWith(requirement.DomainName))
+            {
+                context.Succeed(requirement);
+                return Task.CompletedTask;
+            }
+
+            context.Fail();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ JobOffersPortal.Infrastructure.Security/Authorization/DomainNameForEmail/DomainNameForEmailRequirement.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace JobOffersPortal.Infrastructure.Security.Authorization.DomainNameForEmail
+{
+    public class DomainNameForEmailRequirement : IAuthorizationRequirement
+    {
+        public string DomainName { get; }
+
+        public DomainNameForEmailRequirement(string domainName)
+        {
+            DomainName = domainName;
+        }
+    }
+}
