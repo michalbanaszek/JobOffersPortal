0ae6e76022ba8e83922dea8ebc4b6e37ad45b34f
Michał Banaszek <biaaly13@gmail.com>
2022-03-28 10:05:44 +0200

    update jwtoptions

--- JobOffersPortal.Infrastructure.Security/Options/JwtOptions.cs
+++ JobOffersPortal.Infrastructure.Security/Options/JwtOptions.cs
@@ -6,5 +6,8 @@ namespace JobOffersPortal.Infrastructure.Security.Options
     {
         public string Secret { get; set; }
         public TimeSpan TokenLifeTime { get; set; }
+        public string Issuer { get; set; }
+        public string Subject { get; set; }
+        public string Audience { get; set; }
     }
 }
