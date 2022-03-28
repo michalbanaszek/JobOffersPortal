89569ffea5a2cf264523485898452ee2ba9e388a
Michał Banaszek <biaaly13@gmail.com>
2022-03-28 10:10:44 +0200

    added serilog

--- JobOffersPortal.UI/Program.cs
+++ JobOffersPortal.UI/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
 
 namespace WebApp
 {
@@ -12,6 +15,21 @@ namespace WebApp
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+               .UseSerilog((context, configuration) =>
+               {
+                     configuration.Enrich.FromLogContext()
+                                  .Enrich.WithMachineName()
+                                  .WriteTo.Console()
+                                  .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticSearchOptions:Uri"]))
+                                  {
+                                      IndexFormat = $"{context.Configuration["ApplicationName"]}-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+                                      AutoRegisterTemplate = true,
+                                      NumberOfShards = 2,
+                                      NumberOfReplicas = 1
+                                  })
+                                  .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                                  .ReadFrom.Configuration(context.Configuration);
+               })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
