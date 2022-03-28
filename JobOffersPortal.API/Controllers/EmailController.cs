526e67f0e993ce3a858e0b048f3e86bd2338df83
Michał Banaszek <biaaly13@gmail.com>
2022-03-28 10:03:18 +0200

    added metrics

--- JobOffersPortal.API/Controllers/EmailController.cs
+++ JobOffersPortal.API/Controllers/EmailController.cs
@@ -1,4 +1,6 @@
-﻿using JobOffersPortal.Application.Common.Interfaces;
+﻿using App.Metrics;
+using JobOffersPortal.API.Metrics;
+using JobOffersPortal.Application.Common.Interfaces;
 using JobOffersPortal.Persistance.EF.Contracts;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -10,16 +12,20 @@ namespace JobOffersPortal.API.Controllers
     public class EmailController : ControllerBase
     {
         private readonly IEmailService _emailService;
+        private readonly IMetrics _metrics;
 
-        public EmailController(IEmailService emailService)
+        public EmailController(IEmailService emailService, IMetrics metrics)
         {
             _emailService = emailService;
+            _metrics = metrics;
         }
 
         [HttpPost("Send")]
         public async Task<IActionResult> Send([FromForm] SendEmailRequest request)
         {
             await _emailService.SendEmailAsync(request.ToEmail, request.Subject, request.Content, request.Files);
+
+            _metrics.Measure.Counter.Increment(MetricsSendEmail.SentEmailCounter);
 
             return Ok();
         }
