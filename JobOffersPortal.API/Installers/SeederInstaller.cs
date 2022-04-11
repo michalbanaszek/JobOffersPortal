9f6339eef7d64b95ad1451accf3620ce62082eaa
Michał Banaszek <biaaly13@gmail.com>
2022-04-11 14:54:12 +0200

    added seeder

---
+++ JobOffersPortal.API/Installers/SeederInstaller.cs
@@ -0,0 +1,15 @@
+﻿using JobOffersPortal.Application.Common.Interfaces;
+using JobOffersPortal.Persistance.EF.Persistence;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JobOffersPortal.API.Installers
+{
+    public class SeederInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ApplicationDbContextSeed>();
+        }
+    }
+}
