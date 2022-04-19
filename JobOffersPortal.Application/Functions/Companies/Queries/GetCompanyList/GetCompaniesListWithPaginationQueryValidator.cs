dc08422b2e75a8dcf43cf1da3fb2e6a78c35faff
Michał Banaszek <biaaly13@gmail.com>
2022-04-19 10:43:32 +0200

    validator for pagesize

---
+++ JobOffersPortal.Application/Functions/Companies/Queries/GetCompanyList/GetCompaniesListWithPaginationQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace JobOffersPortal.Application.Functions.Companies.Queries.GetCompanyList
+{
+    public class GetCompaniesListWithPaginationQueryValidator : AbstractValidator<GetCompaniesListWithPaginationQuery>
+    {
+        public GetCompaniesListWithPaginationQueryValidator()
+        {
+            RuleFor(x => x.PageSize).LessThanOrEqualTo(100);
+        }
+    }
+}
