e48080ab0dc597e8d1813a99c89aaa748944d2c0
Michał Banaszek <biaaly13@gmail.com>
2022-04-21 14:59:16 +0200

    added fake classes

---
+++ JobOffersPortal.IntegrationTests/Fakes/FakeUserFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace JobOffersPortal.IntegrationTests.Fakes
+{
+    public class FakeUserFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var claimsPrincipal = new ClaimsPrincipal();
+
+            claimsPrincipal.AddIdentity(new ClaimsIdentity(new[]             
+            {
+                 new Claim("id", "1")
+            }));
+
+            context.HttpContext.User = claimsPrincipal;
+
+            await next();
+        }
+    }
+}
